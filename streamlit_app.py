21003519f868abdfffdfa23aa8eea7228c7c9f0e
rajasekhar064 <149458198+rajasekhar064@users.noreply.github.com>
2023-11-29 16:23:01 +0530

    Update streamlit_app.py
    added the menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
 

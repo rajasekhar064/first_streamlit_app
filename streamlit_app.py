066f8396fb2a06622c6456debce0ca86f6c37a63
rajasekhar064 <149458198+rajasekhar064@users.noreply.github.com>
2023-12-04 10:58:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import snowflake.connector
 import streamlit
 streamlit.title('MY PARENTS NEW HEALTHY DINNER')
 
@@ -33,5 +34,5 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
-import snowflake.connector
+
 
